8ffb62d4f5dc27a18b547f1ffec121e4f88269d3
Paul Gerste <79814126+paul-gerste-sonarsource@users.noreply.github.com>
2025-08-08 19:28:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('Dummy action (JS)');
+console.log('argv:', process.argv);
+console.log('execPath:', process.execPath);
+console.log('argv0:', process.argv0);
+console.log('env:', process.env);
