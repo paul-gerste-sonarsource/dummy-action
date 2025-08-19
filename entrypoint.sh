478d78c46037ffcca43f5c464c577217b06568af
Paul Gerste <79814126+paul-gerste-sonarsource@users.noreply.github.com>
2025-08-19 10:20:05 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
-set -x
+#set -x
 
+echo id
 id
+
+echo /var/run/docker.sock
 ls -halF /var/run/docker.sock
+
+echo /github/
 ls -halF /github/
+
+echo /github/file_commands/
 ls -halF /github/file_commands/
+
+echo /github/home/
 ls -halF /github/home/
+
+echo /github/workflow/
+ls -halF /github/workflow/
+
+echo /github/workspace/
 ls -halF /github/workspace/
+
+echo env
 env | sort
